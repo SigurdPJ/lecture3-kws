3cfeb5e943c0790dd0efe31523fa8fecfa8a8198
Sigurd Jongers <sijo063@student.kristiania.no>
2026-01-18 22:51:02 +0100

    url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // This has to be equal to the name of your repository
-  // For example, since this repository is https://github.com/kristiania-kws2100-2025/kws2100-kartbaserte-websystemer,
-  //  `base` has to be `/kws2100-kartbaserte-websystemer`
-  base: "/kws2100-kartbaserte-websystemer",
+  base: "/lecture3-kws",
 });
