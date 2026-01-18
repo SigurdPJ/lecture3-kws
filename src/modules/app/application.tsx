b4dc4a299ecfcc2cd95b7a9ab3796ec604dea6da
Sigurd Jongers <sijo063@student.kristiania.no>
2026-01-18 23:05:19 +0100

    url

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -5,12 +5,23 @@ import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
 
 useGeographic();
 
 const map = new Map({
   view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    new VectorLayer({
+      source: new VectorSource({
+        url: "/geojson/fylker.json",
+        format: new GeoJSON(),
+      }),
+    }),
+  ],
 });
 
 export function Application() {
