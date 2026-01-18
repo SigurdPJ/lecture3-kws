cc6619bddc87ebfa9e7c821ecf639e7b069a287a
Sigurd Jongers <sijo063@student.kristiania.no>
2026-01-19 00:52:14 +0100

    yup 2

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -3,15 +3,14 @@ import { Feature, Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
 import { OSM } from "ol/source.js";
 import { useGeographic } from "ol/proj.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
+import VectorLayer from "ol/layer/Vector.js";
+import type { FeatureLike } from "ol/Feature.js";
+import { Style } from "ol/style.js";
 
 import "ol/ol.css";
 import "./application.css";
-
-import VectorLayer from "ol/layer/Vector.js";
-import VectorSource from "ol/source/Vector.js";
-import { GeoJSON } from "ol/format.js";
-import { Style, Text } from "ol/style.js";
-import type { FeatureLike } from "ol/Feature.js";
 import { getCenter } from "ol/extent.js";
 
 useGeographic();
@@ -30,25 +29,35 @@ const kommuneSource = new VectorSource({
 const kommuneLayer = new VectorLayer({
   source: kommuneSource,
 });
+
 const layers = [new TileLayer({ source: new OSM() }), kommuneLayer, fylkeLayer];
 const view = new View({ center: [11, 59], zoom: 8 });
 const map = new Map({ layers, view });
 
-function Application() {
-  const mapRef = useRef<HTMLDivElement | null>(null);
+export function Application() {
+  const [selectedKommune, setSelectedKommune] = useState<Feature>();
+
+  function handleClick(event: MapBrowserEvent) {
+    const kommune = kommuneSource.getFeaturesAtCoordinate(event.coordinate);
+    setSelectedKommune(kommune.length > 0 ? kommune[0] : undefined);
+  }
+
   useEffect(() => {
-    map.setTarget(mapRef.current!);
+    map.on("click", handleClick);
   }, []);
 
   const [activeFylke, setActiveFylke] = useState<Feature>();
+
   function activeFylkeStyle(fylke: FeatureLike) {
     const fylkesnavn = fylke.getProperties()["fylkesnavn"];
-    return new Style({ text: new Text({ text: fylkesnavn }) });
+    return new Style({ text: fylke.get("fylkesnavn") || "" });
   }
+
   function handlePointerMove(event: MapBrowserEvent) {
     const fylke = fylkeSource.getFeaturesAtCoordinate(event.coordinate);
     setActiveFylke(fylke.length > 0 ? fylke[0] : undefined);
   }
+
   useEffect(() => {
     activeFylke?.setStyle(activeFylkeStyle);
     return () => activeFylke?.setStyle(undefined);
@@ -57,32 +66,26 @@ function Application() {
     map.on("pointermove", handlePointerMove);
   }, []);
 
-  const [selectedKommune, setSelectedKommune] = useState<Feature>();
-  function handleClick(event: MapBrowserEvent) {
-    const kommune = kommuneSource.getFeaturesAtCoordinate(event.coordinate);
-    setSelectedKommune(kommune.length > 0 ? kommune[0] : undefined);
-  }
-  useEffect(() => {
-    map.on("click", handleClick);
-  }, []);
-
   const [allKommuner, setAllKommuner] = useState<Feature[]>([]);
   useEffect(() => {
     kommuneSource.on("change", () =>
       setAllKommuner(kommuneSource.getFeatures()),
     );
   }, []);
+
   function handleClickKommune(kommune: Record<string, any>) {
     view.animate({ center: getCenter(kommune["geometry"]!.getExtent()) });
   }
 
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+  }, []);
+
   return (
     <>
-      <h1>
-        {selectedKommune
-          ? selectedKommune.getProperties()["kommunenavn"]
-          : "Administrative regioner i Norge"}
-      </h1>
+      <h1>Administrative regioner i Norge </h1>
       <main>
         <div ref={mapRef}></div>
         <aside>
@@ -104,5 +107,3 @@ function Application() {
     </>
   );
 }
-
-export default Application;
